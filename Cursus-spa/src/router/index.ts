6b2f3d61866bbaecf41097625334ee515333683f
jay19240 <aliyah.raijin@gmail.com>
2024-12-27 16:15:05 +0100

    update

--- Cursus-spa/src/router/index.ts
+++ Cursus-spa/src/router/index.ts
@@ -7,7 +7,7 @@ import { creatureResolver } from '@/_resolvers/CreatureResolver';
 // -------------------------------------------------------------
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(),
   routes: [
     {
       path: '/',
