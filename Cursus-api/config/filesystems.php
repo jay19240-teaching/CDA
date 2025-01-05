681e5876ceace85134c3d05c70a1b09687ae5f3a
jay19240 <aliyah.raijin@gmail.com>
2025-01-05 11:32:03 +0100

    update

--- Cursus-api/config/filesystems.php
+++ Cursus-api/config/filesystems.php
@@ -70,8 +70,7 @@ return [
     */
 
     'links' => [
-        public_path('storage') => storage_path('app/public'),
-        public_path('storage/pokemons/images') => storage_path('app/public/pokemons/images')
+        public_path('storage') => storage_path('app/public')
     ],
 
 ];
